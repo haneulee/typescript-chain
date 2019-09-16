950a3c5a3ae17bf74965bdd287baab49b28b7638
Haneul Lee <lovesky4294@gmail.com>
2019-09-16 15:19:43 +0900

    createNewBlock

--- src/index.ts
+++ src/index.ts
@@ -58,8 +58,34 @@ let blockChain: Block[] = [genesisBlock];
 
 const getBlockChain = ():Block[] => blockChain;
 
-const getLatestlock = ():Block => blockChain[blockChain.length - 1];
+const getLatestBlock = ():Block => blockChain[blockChain.length - 1];
 
 const getNewTimestamp = ():number => Math.round(new Date().getTime() / 1000)
+
+const createNewBlock = (data: string): Block => {
+
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimestamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  )
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  )
+
+  return newBlock
+}
+
+
+console.log(createNewBlock("hello"));
+console.log(createNewBlock("bye bye"));
 
 export {}
