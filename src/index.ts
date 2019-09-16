cad413df1d6e4b4187c42ef38bf2a5e709bbcbdf
Haneul Lee <lovesky4294@gmail.com>
2019-09-16 13:48:40 +0900

    classes in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
-interface Human {
-  name: String
-  age: Number
-  gender: String
+class Human {
+  public name: string;
+  public age: number; //private
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "haneul",
-  age: 23,
-  gender: "female"
-}
-
-
+const haneul = new Human("Haneul", 30, "female");
 
 // const sayHi = (name: string, age: number, gender: string): string => {
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
   };
   
-  console.log(sayHi(person));
+  console.log(sayHi(haneul));
   
   export {};
